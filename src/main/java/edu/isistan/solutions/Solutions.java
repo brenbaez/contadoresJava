5d001af673a1c2bb9d786ec85f849d4ad80ea8ac
guido <guidomodarelli25@gmail.com>
2020-06-20 18:47:20 -0300

    Medir el benchmark

--- src/main/java/edu/isistan/solutions/Solutions.java
+++ src/main/java/edu/isistan/solutions/Solutions.java
@@ -13,29 +13,28 @@ public class Solutions {
 
         ProblemGen problemGen = new ProblemGen();
 
-        IProblemSolver naive = new SolutionDevelopmentBinarySearch();
+        IProblemSolver naive = new SolutionNaive();
 
-        int repetitions = 20;
+        int repetitions = 50;
         long[] times = new long[repetitions];
 
         for (int i = 0; i < repetitions; i++) {
-            problemGen.genRandomProblem(1000000);
 
-            long start = System.currentTimeMillis(); //acá no está haciendo el warm up para empezar con el benchmarking!!
-
+            problemGen.genRandomProblem(100000);
             int[] data = problemGen.getData();
             int target = (int) (Math.random() * 2 * Integer.MAX_VALUE + Integer.MIN_VALUE / 2);
+            System.out.printf("Nro iteracion: %d%n", i);
+
+            long start = System.currentTimeMillis();
             List<Pair> pairs = naive.isSumIn(data, target);
-
             System.out.printf(" -- Pairs: %d%n", pairs.size());
-
             start = System.currentTimeMillis() - start;
             System.out.printf("Tiempo total: %d%n", start);
+
             times[i] = start;
         }
 
         long sum = Arrays.stream(times).sum();
-
         System.out.printf("%nTiempo promedio: %d%n", sum / repetitions);
     }
 }
